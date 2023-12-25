3c4cd3f3a4c13c5bfc7fa2217baf09b56320195a
elcrity <elcrity@naver.com>
2023-12-25 15:47:31 +0900

    nodejs server, react client 연동

--- server/src/Server.js
+++ server/src/Server.js
@@ -1,7 +1,13 @@
+const cors = require('cors')
 const express = require('express')
 
 const app = express();
+app.use(cors());
 
 app.listen('8080', function(){
-    console.log('server on 8080port');
+    console.log('server is running on 8080');
+})
+
+app.get('/api', (req, res) => {
+    res.send({message:'hello'})
 })
